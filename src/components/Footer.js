72d7fe975add3be095522a6264913e778f2b8cc8
Cosdaman <Cosdaguy@gmail.com>
2022-02-04 12:55:29 -0800

    sticky footer with phantom div

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Navbar, NavbarBrand, Container } from 'react-bootstrap'
+import '../styles/Footer.css'
 
 export default function Footer() {
   return (
-    <footer>
-      Footer
-    </footer>
+    <div className='phantom'>
+      <div className="fixed-bottom">
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <NavbarBrand>Footer</NavbarBrand>
+          </Container>
+        </Navbar>
+      </div>
+    </div>
   );
 }
 
--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -10,9 +10,14 @@ export default function NavbarComp() {
       <Container>
         <Navbar.Brand href="#home">Kevin Ang</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="#home">About Me</Nav.Link>
+          <Nav.Link href="#features">Portfolio</Nav.Link>
+          <Nav.Link href="#pricing">Contact</Nav.Link>
+        </Nav>
+        <Nav className="ml-auto">
+          <Nav.Link href="#home">Resume</Nav.Link>
+          <Nav.Link href="#features">LinkedIn</Nav.Link>
+          <Nav.Link href="#pricing">Github</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
