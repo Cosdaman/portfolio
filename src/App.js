8c66ce66259ec78e8295a12de7f322093be0edcf
cosdaman <cosdaguy@gmail.com>
2022-02-17 14:19:18 -0800

    removed react helmet

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React from "react";
 import Footer from "./components/Footer";
 import Section from './components/Section'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Helmet } from "react-helmet";
 import './App.css'
 
 
 function App() {
   return (
     <div className="App">
-      <Helmet>
-        <meta name="description" content="Developer Portfolio" />
-      </Helmet>
       <Section />
       <Footer />
-
     </div>
 
   );
