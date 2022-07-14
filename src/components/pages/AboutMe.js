3ac9769f51fb8a9484a0c327f1576e68bce6fb0a
Cosdaman <kjc.ang@gmail.com>
2022-07-14 09:49:49 -0700

    updated pages

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -6,9 +6,6 @@ import '../../styles/AboutMe.css'
 
 export default function AboutMe() {
 
-  const skills = ['Python', 'Git', 'Java', 'MySQL', 'Javascript', 'MongoDB', 'ExpressJS', 'React', 'NodeJS']
-  const skillList = skills.map((element, i) => <li key={i}>{element}</li>)
-
   return (
     <Container className='pt-2'>
       <Row>
@@ -16,24 +13,23 @@ export default function AboutMe() {
           <img className="profilePhoto d-block mx-auto" src={avatar} alt="avatar" />
         </Col>
         <Col className='d-flex flex-column align-items-center' style={{ textAlign: 'center' }}>
-          <p>Hi there! My name is Kevin! </p>
+          <p>Hello there, I'm Kevin. It's nice to meet you. </p>
           <p>You found my webpage! You'll find all sorts of stuff about me here!</p>
           <p>Let me tell you a bit about myself.</p>
         </Col>
       </Row>
       <Row>
         <h1 id='about'>About Me</h1>
-        <p>I have a Bachelors of Science in Computer Engineering from De La Salle University in the Philippines, and a Diploma in Business IT Management from the British Columbia Institute of Technology in Vancouver, Canada.
+        <p>I have a degree in Computer Engineering from DLSU-Manila, a diploma in Business IT Management from BCIT and a certificate in full stack web development from the University of Washington.
         </p>
-        <p> I've also graduated from a full stack web development bootcamp and I'm currently improving my skills in the MERN stack; in addition to existing knowledge and skills in the following: </p>
-        <Row className='skills'>
-          {skillList}
-        </Row>
-        <p>People usually know me as a leader, shot-caller, and passionate problem-solver within a team.</p>
-        <p>Each project that I am involved with is aimed to have innovative, and effective solutions towards
-          problems at hand.</p>
-        <p>I am looking forward to the day I use my skills to create and engineer functional solutions while ensuring the utmost
-          quality and performance for those involved.</p>
+        <p>Currently, I've been working for about half a year as a Teaching Assistant at a full stack web developer program, with an average class size of 40 students. I utilize my knowledge in the MERN stack to support students and troubleshoot any issues they may have. </p>
+        <p>I've also been studying Data Engineering with Per Scholas to expand my skill set in various technologies such as python and linux; and techniques in data manipulation, data warehousing, and data visualization.
+        </p>
+
+        <p>I love creating solutions and constantly improving processes and  I'd love the opportunity to be able to work with others to help achieve goals and solve problems.</p>
+        <p>I'd love to connect on <a href="https://www.linkedin.com/in/kevin-jc-ang/">LinkedIn</a> discuss any potential and upcoming opportunities. I look forward to answering any questions you might have for me.</p>
+        <p>Thank you for taking the time to read!</p>
+
       </Row>
     </Container>
   );
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
   return (
     <Container className='d-flex flex-column mt-2 justify-content-center align-items-center'>
       <h1>Contact Me</h1>
-      <p>Feel free to contact me through the following avenues:</p>
+      <p>Feel free to contact me regarding projects and opportunites through the following methods:</p>
       <p>E-mail: <a href="mailto:kjc.ang@gmail.com">kjc.ang@gmail.com</a></p>
 
     </Container>
